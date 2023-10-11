8b401f64bb4925f72085b4e4fa4e71d3713d8e46
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-10-11 22:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	let start = 0, end = str.length-1;
 
+	while(start < end){
+		if(str[start++] != str[end--]){
+			return false; // not a palindrome
+		}
+	}
+	return true; // its a palindrome
 }
 module.exports = palindrome
